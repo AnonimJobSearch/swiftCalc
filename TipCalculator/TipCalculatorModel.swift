4bc4ce71d3feca571cca61994ded60352ff79554
pavel <pavel>
2016-07-12 21:15:57 +0300

    commit from Xcode

--- TipCalculator/TipCalculatorModel.swift
+++ TipCalculator/TipCalculatorModel.swift
@@ -19,8 +19,7 @@ class TipCalculatorModel {
             return total / (taxPct + 1)
         }
         set(newSubtotal) {
-            //kugkg
-        }
+                 }
     }
     
     // 3
@@ -40,6 +39,18 @@ class TipCalculatorModel {
         print("15%: \(calcTipWithTipPct(0.15))")
         print("18%: \(calcTipWithTipPct(0.18))")
         print("20%: \(calcTipWithTipPct(0.20))")
+    }
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
     }
     
 }
